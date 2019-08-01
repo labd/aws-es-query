02561356da2a720fe20e34ca20ac4dccade4f773
Michael van Tellingen <michael@mvantellingen.nl>
2019-08-01 11:34:53 +0200

    Add -X<Method> support

--- aws_es_query.py
+++ aws_es_query.py
@@ -9,8 +9,9 @@ from requests_aws4auth import AWS4Auth
 
 @click.command()
 @click.option("--role-arn", required=True, type=str)
+@click.option("-X", required=False, type=str, default="GET")
 @click.argument("url")
-def main(role_arn, url):
+def main(role_arn, x, url):
     sts = boto3.client("sts")
     resp = sts.assume_role(RoleArn=role_arn, RoleSessionName="es-curl")
     awsauth = AWS4Auth(
@@ -21,7 +22,7 @@ def main(role_arn, url):
         session_token=resp["Credentials"]["SessionToken"],
     )
 
-    response = requests.get(url, auth=awsauth)
+    response = requests.request(x, url, auth=awsauth)
 
     try:
         print(json.dumps(response.json(), indent=4, sort_keys=True))
